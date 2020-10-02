e0f0e207db686e47060d642c6cf86a4468006829
Edward Fabian Baron <edwardbaron99@gmail.com>
2020-10-01 22:13:01 -0500

    add: carpeta docs para github

---
+++ docs/precache-manifest.005bbc83238c052e5e17f43c28262394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8068ec3fed548ba6f796d21aee829dbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6a6701f506b581aebe",
+    "url": "/static/css/main.f5bd0d97.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6a6701f506b581aebe",
+    "url": "/static/js/main.6b69d435.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
