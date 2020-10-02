19cc4239d8aa691431c23bada1389c740bcc5402
Edward Fabian Baron <edwardbaron99@gmail.com>
2020-10-01 22:26:19 -0500

    fix: borrar texto buscado

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -13,6 +13,7 @@ export const AddCategory = ({ setCategories }) => {
     e.preventDefault();
     if (inputValue.trim().length >= 2) {
       setCategories(cat => [inputValue, ...cat]);
+      setInputValue('');
     }
   }
 
